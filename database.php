aea515282ae79a8d900649fbf40b4d162ea87495
Utsav Thakur <77831507+utsav1-cyber@users.noreply.github.com>
2021-03-04 16:53:41 +0545

    Add files via upload

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+//connect to mysql
+$con = mysqli_connect("localhost", "root", "", "shoutit");
+//test connection
+if (mysqli_connect_errno()) {
+echo 'Failed to connect to MySQL: '.mysqli_connect_error();
+}
